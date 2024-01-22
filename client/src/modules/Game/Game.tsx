de7299d8b68e2f771762053201ec69995be950ee
Константин <kostyagreduagin.ru@gmail.com>
2024-01-22 19:02:42 +0400

    start

---
+++ client/src/modules/Game/Game.tsx
@@ -0,0 +1,58 @@
+import React, { useCallback, useContext, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { ServerContext, MediatorContext } from "../../App";
+import { TGamer, TMobs, TScene } from "../Server/types";
+import Player from "./Player";
+import Scene from "./Scene";
+import { Physics } from "@react-three/rapier";
+import Friends from "./Friends";
+import Boss from "./Boss";
+import Bullets from "./Bullets";
+const Game: React.FC = () => {
+    const server = useContext(ServerContext);
+    const mediator = useContext(MediatorContext);
+    const { GET_SCENE } = mediator.getEventTypes();
+    const [infoFriends, setInfoFriends] = useState<TGamer[] | null>(null);
+    const [infoMobs, setInfoMobs] = useState<TMobs[] | null>(null);
+
+    useEffect(() => {
+        server.startGameInterval();
+
+        const getSceneHandler = (scene: TScene) => {
+            if (scene.gamers != null) {
+                setInfoFriends(scene.gamers);
+            }
+            if (scene.mobs != null) {
+                setInfoMobs(scene.mobs);
+            }
+        };
+        mediator.subscribe(GET_SCENE, getSceneHandler);
+
+        return () => {
+            mediator.unsubscribe(GET_SCENE, getSceneHandler);
+            server.stopGameInterval();
+        };
+    });
+
+    return (
+        <Canvas
+            camera={{
+                position: [0, 0, 14],
+                zoom: 65,
+                near: 0.1,
+                far: 1000,
+            }}
+            orthographic
+        >
+            <ambientLight intensity={2} position={[0, 0, 5]} />
+            <Physics gravity={[0, 0, -10]}>
+                <Scene />
+                <Player />
+                <Friends infoFriends={infoFriends} />
+                <Boss />
+                <Bullets infoFriends={infoFriends} infoMobs={infoMobs} />
+            </Physics>
+        </Canvas>
+    );
+};
+export default Game;
---
+++ server/application/modules/DB.php
@@ -0,0 +1,141 @@
+<?php
+class DB {
+    private $db;
+
+    function __construct($config) {
+        extract($config);
+
+        $this->db = new PDO("mysql:host=$host;port=$port;dbname=$db", $user, $pass);
+
+    }
+
+    function __destruct() {
+        $this->db = null;
+    }
+
+    private function preparationQuery($query, $arr = []) {
+        $stmt = $this->db->prepare($query);
+        $stmt->execute($arr);
+        return $stmt;
+    }
+
+    function getPersons($token) {
+        $query = 'SELECT * FROM persons';
+        $stmt = $this->db->query($query);
+        return $stmt->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    function getUserById($id) {
+        $query = 'SELECT * FROM users WHERE id=?';
+        return $this->preparationQuery($query, [$id])->fetch(PDO::FETCH_OBJ);
+    }
+
+    function getUserByLogin($login) {
+        $query = 'SELECT * FROM users WHERE login=?';
+        return $this->preparationQuery($query, [$login])->fetch(PDO::FETCH_OBJ);
+    }
+
+    function getUserByToken($token) {
+        $query = 'SELECT * FROM users WHERE token=?';
+        return $this->preparationQuery($query, [$token])->fetch(PDO::FETCH_OBJ);
+    }
+
+    function addUser($login, $nickname, $password) {
+        $query = 'INSERT INTO users (login, name, password ) VALUES (?, ?, ?)';
+        $this->preparationQuery($query, [$login, $nickname, $password]);
+    }
+
+    function updateToken($userId, $token) {
+        $query = 'UPDATE users SET token=? WHERE id=?';
+        $this->preparationQuery($query, [$token, $userId]);
+    }
+
+    function getItems(){
+        $query = 'SELECT * FROM items';
+        $stmt = $this->db->query($query);
+        return $stmt->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    function sendMessage($userId, $message) {
+        $query = 'INSERT INTO messages (user_id, message, created) VALUES (?, ?, now())';
+        $this->preparationQuery($query, [$userId, $message]);
+    }
+
+    function getMessages() {
+        $query = 'SELECT 
+                m.message AS message,
+                u.name AS name
+            FROM messages AS m
+            INNER JOIN users AS u
+            ON u.id=m.user_id
+            WHERE m.created >= DATE_SUB(NOW(), INTERVAL 1 DAY)
+            ORDER BY m.created DESC';
+        return $this->preparationQuery($query, [])->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    function updateChatHash($hash) {
+        $query = 'UPDATE game SET chat_hash=? WHERE id=1';
+        $this->preparationQuery($query, [$hash]);
+    }
+
+    function updateGamersHash($hash) {
+        $query = 'UPDATE game SET gamers_hash=? WHERE id=1';
+        $this->preparationQuery($query, [$hash]);
+    }
+    function updateMobsHash($hash) {
+        $query = 'UPDATE game SET mobs_hash=? WHERE id=1';
+        $this->preparationQuery($query, [$hash]);
+    }
+
+    function updateTimestamp($updateTimestamp) {
+        $query = 'UPDATE game SET update_timestamp=? WHERE id=1';
+        $this->preparationQuery($query, [$updateTimestamp]);
+    }
+
+    function getHashes() {
+        $query = 'SELECT * FROM game WHERE id=1';
+        return $this->preparationQuery($query, [])->fetch(PDO::FETCH_OBJ);
+    }
+
+    function getGamers() {
+        $query = 'SELECT 
+            u.name AS name,
+            g.person_id AS person_id,
+            g.status AS status,
+            g.x AS x,
+            g.y AS y,
+            g.direction AS direction
+        FROM gamers AS g
+        INNER JOIN users AS u
+        ON u.id=g.user_id';
+        return $this->preparationQuery($query, [])->fetchAll(PDO::FETCH_OBJ);
+    }
+    function getMobs() {
+        $query = 'SELECT * FROM mobs';
+        return $this->preparationQuery($query, [])->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    function move($userId, $direction, $x, $y, $status) {
+        $query = 'UPDATE gamers SET direction=?, x=?, y=?, status=? WHERE user_id=?';
+        $this->preparationQuery($query, [$direction, $x, $y, $status,$userId]);
+    }
+    function moveMobs($x, $y) {
+        $query = 'UPDATE mobs SET x=?, y=? WHERE id=1';
+        $this->preparationQuery($query, [$x, $y]);
+    }
+
+    function updatePersonId($userId, $newPersonId) {
+        $query = 'UPDATE gamers SET person_id=? WHERE id=?';
+        $this->preparationQuery($query, [$newPersonId, $userId]);
+    }
+
+    function deleteGamers(){
+        $query = 'TRUNCATE TABLE gamers';
+        $this->preparationQuery($query, []);
+    }
+
+    function AddGamers() {
+        $query = 'INSERT INTO gamers (user_id, person_id, status, x, y, direction) VALUES (?, ?, "stand", 0, 0, "down")';
+        $this->preparationQuery($query, []);
+    }
+}
